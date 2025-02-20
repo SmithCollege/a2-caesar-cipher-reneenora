78b867a7b7cb3038cd8f24bb7144fd3097ce324b
norazhou0 <nzhou@smith.edu>
2025-02-19 20:13:41 -0500

    added our tests

--- app/src/test/java/a1template/YourTests.java
+++ app/src/test/java/a1template/YourTests.java
@@ -6,6 +6,23 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class YourTests {
+    // test with negative shifts
+    @Test
+    public void encodeTest(){
+        CaesarCipher classUnderTest = new CaesarCipher(-8);
+        assertEquals("bpqa qa i amkzmb umaaiom", classUnderTest.encode("this is a secret message"));
+        
+        classUnderTest = new CaesarCipher(-3);
+        assertEquals("wklv lv d whvw phvvdjh.", classUnderTest.encode("this isn't very secret."));
+    }
 
+    @Test
+    public void decodeTest(){
+        CaesarCipher classUnderTest = new CaesarCipher(-8);
+        assertEquals("this is a secret message", classUnderTest.decode("bpqa qa i amkzmb umaaiom"));
+        
+        classUnderTest = new CaesarCipher(-3);
+        assertEquals("this isn't very secret.", classUnderTest.decode("wklv lv d whvw phvvdjh."));
 
+    }
 }
